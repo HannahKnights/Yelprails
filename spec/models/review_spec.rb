e08a23d5d3898af7e49df61ed2b0d7f21f592c33
Hannah Knights <hannahknights@hotmail.co.uk>
2014-01-14 17:30:43 +0000

    Added average review rating feature

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
-describe Review do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe '.average_rating' do
+  
+  let(:restaurant) {Restaurant.new(name: 'La Scala', description: 'Italian food')}
+
+  context 'with no ratings' do
+  
+    it 'returns "Unrated"' do
+      expect(restaurant.average_rating).to eq 'Unrated'
+    end
+
+  end
+
+  context 'with one rating' do
+
+    it 'returns the rating' do
+      restaurant.reviews << Review.new(rating: 5)
+      expect(restaurant.average_rating).to eq 5
+    end
+  
+  end
+
+  context 'with more than one rating' do
+
+    it 'returns the average' do
+      restaurant.reviews << Review.new(rating: 5)
+      restaurant.reviews << Review.new(rating: 3)
+      expect(restaurant.average_rating).to eq 4
+    end
+  
+  end
+
 end
+
+
+
