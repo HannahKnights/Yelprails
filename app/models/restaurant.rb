228f2aa465282a1541807c20addf557fb0fe8e50
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-01-15 13:57:43 +0000

    Refactoring code and moving form elements into partials

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,15 +1,13 @@
 class Restaurant < ActiveRecord::Base
 
   has_many :reviews
-  
+
   validates :name, presence: true
   validates :description, presence: true
 
 
   def average_rating
-    return 'Unrated' if reviews.none?
-    total = reviews.inject(0) {|sum, review| sum += review.rating}
-    total/reviews.length
+    reviews.average(:rating) || 'Unrated'
   end
 
 end
